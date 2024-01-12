efc78d6f2757bb9d6c3aea8f53eea6f213d01125
pari-me <144593250+pari-me@users.noreply.github.com>
2024-01-12 08:43:03 +0700

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a href="/" target="_blank">
+          Parisa Karimi
+        </a>{" "}
+        and is {""}
+        <a href="https://github.com/pari-me/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
