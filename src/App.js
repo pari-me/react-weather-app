5ff2da5e117c362759c7a12e653fc9c27be1298c
pari-me <144593250+pari-me@users.noreply.github.com>
2024-01-12 09:05:41 +0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by {""}
-        <a href="/" target="_blank">
-          Parisa Karimi
-        </a>{" "}
-        and is {""}
-        <a href="https://github.com/pari-me/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by {""}
+          <a href="/" target="_blank" rel="noreferrer">
+            Parisa Karimi
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/pari-me/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
